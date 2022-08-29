38d973c17f4e5a9de6ad1378efe5184a88565c09
OrlandoOroporte <orlando.oroporte@gmail.com>
2022-08-29 23:05:44 +0000

    primer cambio

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet, People, Favorites
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,82 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# @app.route('/user', methods=['POST'])
+# def add_new_user():
+#     request_body = request.json
+#     User.append(request_body)
+#     return jsonify(User)
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+# @app.route('/user/<int:id>', methods=['GET'])
+# def get_user(id):
+#     user = User.query.get(id)
+#     if user is None:
+#         return jsonify({"User does not exist "})
+#     return jsonify(user.serialize()), 200
 
-    return jsonify(response_body), 200
+# @app.route('/planet/<int:id>', methods=['GET'])
+# def get_planet(id):
+#     planet = Planet.query.get(id)
+#     if planet is None:
+#         return jsonify({"Planet does not exist "})
+#     return jsonify(planet.serialize()), 200
+       
+# @app.route('/people/<int:id>', methods=['GET'])
+# def get_people(id):
+#     people = People.query.get(id)
+#     if people is None:
+#         return jsonify({"People does not exist "})
+#     return jsonify(People.serialize()), 200
+
+# @app.route('/user', methods=['GET'])
+# def get_users():
+#     users = User.query.all()
+#     users2 = list(map(lambda user: User.serialize(), users))
+#     return jsonify(users), 200
+
+# @app.route('/planet', methods=['GET'])
+# def get_planets():
+#     planets = Planet.query.all()
+#     planets2 = list(map(lambda planet: Planet.serialize(), planets))
+#     return jsonify(planets), 200
+
+# @app.route('/people', methods=['GET'])
+# def get_peoples():
+#     peoples = People.query.all()
+#     peoples2 = list(map(lambda people: People.serialize(), peoples))
+#     return jsonify(peoples), 200
+
+# @app.route('user/favorites', methods=['GET'])
+# def get_user_favorites():
+#     favorites1 = Favorites.query.all()
+#     favorites2 = list(map(lambda  favorites: Favorites.serialize(), favorites1))
+#     return jsonify(favorites1)
+
+# @app.route('favorite/planet/<int:id>', methods=['POST'])
+# def add_planet_favorites(id):
+
+#     return jsonify()
+
+# @app.route('favorite/people/<int:id>', methods=['POST'])
+# def add_people_favorites(id):
+#     return jsonify()
+
+# @app.route('user/favorites/<int:id>', methods=['DELETE'])
+# def rem_favorites(id):
+#     favorites = Favorites.query.get(id)
+#     db.session.delete(favorites)
+#     db.session.commit()
+#     return 'Delete'
+
+
+# @app.route('/user', methods=['GET'])
+# def handle_hello():
+
+#     response_body = {
+#         "msg": "Hello, this is your GET /user response "
+#     }
+
+#     return jsonify(response_body), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,16 +4,78 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(250), nullable=False, unique=True)
+    email = db.Column(db.String(250), nullable=False, unique=True)
+    password = db.Column(db.String(259), nullable=False)
+    favorites = db.relationship('Favorites', backref='user', uselist=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "username" : self.username,
+            "email": self.email
             # do not serialize the password, its a security breach
+        }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250),nullable=False,unique=True)
+    size = db.Column(db.String(250),nullable=False)
+    population = db.Column(db.String(250),nullable=False)
+    surface = db.Column(db.String(250),nullable=False)
+
+    def __repr__(self):
+        return '<Planet %r>' % self.id
+    
+    def serialize(self):
+        return {
+            'id' : self.id,
+            'name' : self.name,
+            'size' : self.size,
+            'population' : self.population,
+            'surface' : self.surface,
+        }
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250),nullable=False,unique=True)
+    color_eyes = db.Column(db.String(250),nullable=False)
+    color_hair = db.Column(db.String(250),nullable=False)
+    height = db.Column(db.String(250),nullable=False)
+
+    def __repr__(self):
+        return '<People %r>' % self.id 
+    
+    def serialize(self):
+        return {
+            'id' : self.id,
+            'name' : self.name,
+            'color_eyes' : self.color_eyes, 
+            'color_hair' : self.color_hair,
+            'heigh' : self.height
+        }
+
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nature = db.Column(db.String(250), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    naturte_id = db.Column(db.Integer, nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+    __table_args__=(db.UniqueConstraint(
+        'user_id',
+        'name',
+        name='message_unique'
+    ),)
+
+  
+    def __repr__(self):
+        return '<Favorites %r>' % self.id 
+
+    def serialize(self):
+        return {
+            'id' : self.id,
+           
         }
